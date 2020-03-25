a549c12913e736a7be5267af31cf535a1bcf50c8
Keith Dingle <kdingle88@yahoo.com>
2020-03-25 15:36:39 -0500

    Parse csv data into array hashes passed into StatTracker instance

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,7 +1,27 @@
+require 'csv'
+
 class StatTracker
+  attr_reader :games, :teams, :game_teams 
+  def initialize(games, teams, game_teams)
+    @games = games
+    @teams = teams
+    @games_teams = game_teams
+  end
+
   def self.from_csv(locations)
-    stat_tracker = StatTracker.new
+
+    games_data = CSV.read(locations[:games], { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all})
+    teams_data = CSV.read(locations[:teams], { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all})
+    game_teams_data = CSV.read(locations[:game_teams], { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all})
+
+    games = games_data.map {|d| d.to_hash}
+    teams = teams_data.map {|d| d.to_hash}
+    game_teams = game_teams_data.map {|d| d.to_hash}
+    
+
+    stat_tracker = StatTracker.new(games, teams, game_teams)
 
     stat_tracker
   end
+  
 end
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,9 +1,9 @@
 require 'rspec'
 require './lib/stat_tracker'
 
-game_path = './fixtures/game.csv'
-team_path = './fixtures/team_info.csv'
-game_teams_path = './fixtures/game_teams_stats.csv'
+game_path = './spec/fixtures/game.csv'
+team_path = './spec/fixtures/team_info.csv'
+game_teams_path = './spec/fixtures/game_teams_stats.csv'
 
 locations = {
   games: game_path,
@@ -17,6 +17,5 @@ RSpec.describe StatTracker do
     it 'returns an instance of StatTracker.' do
       expect(StatTracker.from_csv(locations)).to be_instance_of(StatTracker)
     end
-    
   end
 end
